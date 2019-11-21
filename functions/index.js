ff5b321ecdca0ff3ff383e247adcf047e6f29338
hlotfy <hlotfy@wellesley.edu>
2019-11-21 12:18:57 -0500

    got the new annotations to appear when submitted

--- functions/index.js
+++ functions/index.js
@@ -23,3 +23,64 @@ exports.setupUsers = functions.auth.user().onCreate(async (user) => {
     id: user.uid
   });
 });
+
+// exports.database.https.onRequest(async (req, res) => {
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // Push the new message into the Realtime Database using the Firebase Admin SDK.
+//   const snapshot = await admin.database().ref('/messages').push({original: original});
+//   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//   res.redirect(303, snapshot.ref.toString());
+// });
+
+// function getArticle(url) {
+//   var article;
+//   fetch(url)
+//     .then(response=> response.json().then(function(response){
+//        console.log(response);
+//        var text = response['response']['content']['fields']['body'];
+//        article = stripHTML(text);
+//       //  for (let i = 0; i < blurbs.length; i++) {
+//       //   if (blurbs[i].trim()!='') {
+//       //     linebreak = document.createElement("br");
+//       //     newBlurb = document.createTextNode(blurbs[i]);
+//       //     allNewsArticlesFromApi.appendChild(newBlurb);
+//       //     allNewsArticlesFromApi.appendChild(linebreak);
+//       //   }
+//       //  }
+//       return article;
+//     }));
+//     return article;
+//   }
+
+
+function stripHTML(html){
+  // Create a new div element
+  var parsed = html.replace(/<[^>]+>/g, '').split('\n');
+  return parsed;
+}
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.database().ref('/messages').push({original: original});
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
+});
+
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
+    .onCreate((snapshot, context) => {
+      // Grab the current value of what was written to the Realtime Database.
+      const original = snapshot.val();
+      console.log('Uppercasing', context.params.pushId, original);
+      const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      return snapshot.ref.parent.child('uppercase').set(uppercase);
+    });
